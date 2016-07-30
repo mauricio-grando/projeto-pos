dc6530e115f4b91e796e3cf9d7fa1b9811d9e9e7
Grando <mauricioa.grando@gmail.com>
2016-07-30 12:02:25 -0300

    Projeto Zend 2

---
+++ module/Application/src/Application/Controller/ProdutoController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class ProdutoController extends AbstractActionController {
+
+    public function visualizarAction() {
+        return new ViewModel();
+    }
+
+    public function listarAction() {
+        return new ViewModel();
+    }
+
+    public function cadastrarAction() {
+        return new ViewModel();
+    }
+
+    public function excluirAction() {
+        return new ViewModel();
+    }
+
+}
